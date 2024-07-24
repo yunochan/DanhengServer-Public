196268921b5fada990d4c49bcc3a80c171caf54f
yunaochan <2640910163@qq.com>
2024-07-24 22:59:50 +0800

    ada

--- Common/Database/Account/AccountHelper.cs
+++ Common/Database/Account/AccountHelper.cs
@@ -18,7 +18,7 @@ namespace EggLink.DanhengServer.Database.Account
             int newUid = uid;
             if (uid == 0)
             {
-                newUid = 100000001;  // start from 100000001
+                newUid = 1;  // start from 1
                 while (AccountData.GetAccountByUid(newUid) != null)
                 {
                     newUid++;
