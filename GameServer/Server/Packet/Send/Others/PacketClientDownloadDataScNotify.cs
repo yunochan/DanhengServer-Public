390db550859efefefb2d04e28be5cb24ac490404
yunaochan <2640910163@qq.com>
2024-08-02 22:58:20 +0800

    12

---
+++ GameServer/Server/Packet/Send/Others/PacketClientDownloadDataScNotify.cs
@@ -0,0 +1,31 @@
+﻿using EggLink.DanhengServer.Game.Player;
+using EggLink.DanhengServer.Proto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EggLink.DanhengServer.Server.Packet.Send.Others
+{
+    public class PacketClientDownloadDataScNotify : BasePacket
+    {
+        public PacketClientDownloadDataScNotify(byte[] data, PlayerInstance player) : base(CmdIds.ClientDownloadDataScNotify)
+        {
+
+            var clientDownloadData = new ClientDownloadData
+            {
+                Data = Google.Protobuf.ByteString.CopyFrom(data),
+                Version = 81,
+                Time = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+            };
+
+            var proto = new ClientDownloadDataScNotify
+            {
+                DownloadData = clientDownloadData
+            };
+
+            SetData(proto);
+        }
+    }
+}
--- GameServer/Server/Packet/Send/Others/PacketServerAnnounceNotify.cs
+++ GameServer/Server/Packet/Send/Others/PacketServerAnnounceNotify.cs
@@ -10,35 +10,21 @@ namespace EggLink.DanhengServer.Server.Packet.Send.Others
 {
     public class PacketServerAnnounceNotify : BasePacket
     {
-        private static readonly Logger Logger = new("PacketServerAnnounceNotify");
         public PacketServerAnnounceNotify() : base(CmdIds.ServerAnnounceNotify)
         {
             var proto = new ServerAnnounceNotify();
 
-            var beginTime = Extensions.GetUnixSec();
-            var endTime = beginTime + 3600;
-            var configId = 1;
-            var announceContent = ConfigManager.Config.ServerOption.ServerAnnounce.AnnounceContent;
             proto.AnnounceDataList.Add(new AnnounceData()
             {
-                BeginTime = beginTime,
-                EndTime = endTime,
-                ConfigId = (uint)configId,
-                CHJPFPLHJBJ = announceContent,
+                BeginTime = Extensions.GetUnixSec(),
+                EndTime = Extensions.GetUnixSec() + 3600,
+                ConfigId = 1,
+                CHJPFPLHJBJ = ConfigManager.Config.ServerOption.ServerAnnounce.AnnounceContent,
             });
-                Logger.Debug("Announcement is enabled");
-                Logger.Debug($"BeginTime={beginTime}");
-                Logger.Debug($"EndTime={endTime}");
-                Logger.Debug($"ConfigId={configId}");
-                Logger.Debug($"CHJPFPLHJBJ={announceContent}");
 
             if (ConfigManager.Config.ServerOption.ServerAnnounce.EnableAnnounce)
             {
                 SetData(proto);
-            }
-            else 
-            {
-                Logger.Debug("Announcement is diabled");
             }
         }
     }
