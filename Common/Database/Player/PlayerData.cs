20a08c4440cdd7373f00e63d30fbe6a2e3c74c31
yunaochan <2640910163@qq.com>
2024-08-02 23:12:08 +0800

    12

--- Common/Database/Player/PlayerData.cs
+++ Common/Database/Player/PlayerData.cs
@@ -11,8 +11,8 @@ namespace EggLink.DanhengServer.Database.Player
     [SugarTable("Player")]
     public class PlayerData : BaseDatabaseDataHelper
     {
-        public string? Name { get; set; } = "开拓者";
-        public string? Signature { get; set; } = "A Personal Server";
+        public string? Name { get; set; } = "倒卖狗全家死光光";
+        public string? Signature { get; set; } = "官方QQ群 929259728";
         public int Birthday { get; set; } = 0;
         public int CurBasicType { get; set; } = 8001;
         public int HeadIcon { get; set; } = 208001;
--- GameServer/Server/Packet/Send/Others/PacketClientDownloadDataScNotify.cs
+++ GameServer/Server/Packet/Send/Others/PacketClientDownloadDataScNotify.cs
@@ -13,14 +13,14 @@ namespace EggLink.DanhengServer.Server.Packet.Send.Others
         public PacketClientDownloadDataScNotify(byte[] data, PlayerInstance player) : base(CmdIds.ClientDownloadDataScNotify)
         {
 
-            var clientDownloadData = new ClientDownloadData
+            var clientDownloadData = new ClientDownloadData ()
             {
                 Data = Google.Protobuf.ByteString.CopyFrom(data),
                 Version = 81,
                 Time = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
             };
 
-            var proto = new ClientDownloadDataScNotify
+            var proto = new ClientDownloadDataScNotify ()
             {
                 DownloadData = clientDownloadData
             };
