080eda1619a96985b87596283475a5cf3b4c1d12
yunaochan <2640910163@qq.com>
2024-08-10 23:09:59 +0800

    add http status quest

---
+++ WebServer/Controllers/StatusServerRoutes.cs
@@ -0,0 +1,17 @@
+﻿using EggLink.DanhengServer.WebServer.Handler;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EggLink.DanhengServer.WebServer.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class StatusServerRoutes
+    {
+        [HttpGet("/status/server")]
+        public ActionResult<string> StatusServer()
+        {
+            var responseJson = new StatusServerHandler().ResponseJson;
+            return Ok(responseJson);
+        }
+    }
+}
