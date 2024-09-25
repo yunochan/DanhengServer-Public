361d8cb1c60128f132c73d79e6498192586f75db
yunaochan <2640910163@qq.com>
2024-09-25 11:56:48 +0800

    1

---
+++ GameServer/Server/Packet/CmdIds.cs
@@ -0,0 +1,11 @@
+﻿namespace EggLink.DanhengServer.Server.Packet
+{
+    public class CmdIds
+    {
+        // None
+        public const int None = 0;
+
+        // Packet
+        public const int TakeMailAttachmentCsReq = 839;
+    }
+}
