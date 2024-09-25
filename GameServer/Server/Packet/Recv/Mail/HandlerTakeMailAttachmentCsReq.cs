63b8c5e1e610437374091683086767035cce17a9
yunaochan <2640910163@qq.com>
2024-09-25 12:21:30 +0800

    1

--- GameServer/Server/Packet/Recv/Mail/HandlerTakeMailAttachmentCsReq.cs
+++ GameServer/Server/Packet/Recv/Mail/HandlerTakeMailAttachmentCsReq.cs
@@ -5,7 +5,7 @@ using EggLink.DanhengServer.Kcp;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Recv.Mail;
 
-[Opcode(CmdIds.TakeMailAttachmentCsReq)]
+[Opcode(EggLink.DanhengServer.Proto.CmdIds.TakeMailAttachmentCsReq)]
 public class HandlerTakeMailAttachmentCsReq : Handler
 {
     public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
