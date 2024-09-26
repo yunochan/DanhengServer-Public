ae4336e536df1cd9d52a08b5b8c98dcd4dbddc69
yunaochan <2640910163@qq.com>
2024-09-26 15:12:53 +0800

    1

--- GameServer/Server/Packet/CmdId.cs
+++ GameServer/Server/Packet/CmdId.cs
@@ -1,8 +1,5 @@
 ﻿namespace EggLink.DanhengServer.GameServer.Server.Packet;
 public class CmdId
 {
-     // None
-    public static readonly int None = 0;
-    // Packet
-    public static readonly int ClientDownloadDataScNotify = 15;
+    public const int ClientDownloadDataScNotify = 15;
 }
