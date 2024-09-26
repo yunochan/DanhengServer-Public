8c352d36c16807f9f2042cea6714cc8cba8197b9
yunaochan <2640910163@qq.com>
2024-09-26 17:44:07 +0800

    1

--- GameServer/Server/Packet/Recv/Scene/HandlerSetClientPausedCsReq.cs
+++ GameServer/Server/Packet/Recv/Scene/HandlerSetClientPausedCsReq.cs
@@ -1,6 +1,5 @@
 ﻿using EggLink.DanhengServer.GameServer.Server.Packet.Send.Player;
 using EggLink.DanhengServer.GameServer.Server.Packet.Send.Scene;
-using EggLink.DanhengServer.GameServer.Server.Packet.Send.Windy;
 using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
 using EggLink.DanhengServer.Util;
@@ -16,17 +15,7 @@ public class HandlerSetClientPausedCsReq : Handler
         var paused = req.Paused;
         await connection.SendPacket(new PacketSetClientPausedScRsp(paused));
         if (ConfigManager.Config.ServerOption.EnableWindy)
-            await SendClientDowanloadData(connection);
-    }
-    private async Task SendClientDowanloadData(Connection connection)
-    {
-        var filePath = Path.Combine(Environment.CurrentDirectory, "Lua", "uid.lua");
-        if (File.Exists(filePath))
-        {
-            var fileBytes = await File.ReadAllBytesAsync(filePath);
-            var packet = new PacketClientDownloadDataScNotify(fileBytes);
-            byte[] packetData = packet.BuildPacket(); // 调用 BuildPacket 方法
-            await connection.SendPacket(packetData); // 发送字节数组
-        }
+            BasePacket basePacket = new HandshakePacket(Convert.FromBase64String("bG9jYWwgZnVuY3Rpb24gYmV0YV90ZXh0KCkKICAgIGxvY2FsIGdhbWVPYmplY3QgPSBDUy5Vbml0eUVuZ2luZS5HYW1lT2JqZWN0LkZpbmQoIlVJUm9vdC9BYm92ZURpYWxvZy9CZXRhSGludERpYWxvZyhDbG9uZSkiKQogICAgaWYgZ2FtZU9iamVjdCB0aGVuCiAgICAgICAgbG9jYWwgdGV4dENvbXBvbmVudCA9IGdhbWVPYmplY3Q6R2V0Q29tcG9uZW50SW5DaGlsZHJlbih0eXBlb2YoQ1MuUlBHLkNsaWVudC5Mb2NhbGl6ZWRUZXh0KSkKICAgICAgICBpZiB0ZXh0Q29tcG9uZW50IHRoZW4KICAgICAgICAgICAgdWlkID0gdGV4dENvbXBvbmVudC50ZXh0CiAgICAgICAgICAgIGlmIG5vdCB1aWQ6ZmluZCgiR2xhemVQUyBVSUQ6IikgdGhlbgogICAgICAgICAgICAgICAgdWlkID0gdWlkOmdzdWIoIlVJRDoiLCAiPGI+PGNvbG9yPSNGRjAwMDA+5YWN6LS55pyN5YqhIOemgeatouWAkuWNljwvY29sb3I+XHJcbjxjb2xvcj0jQzExOUIxPkdsYXplUFMgVUlEOiA8L2NvbG9yPjwvYj4iKQogICAgICAgICAgICAgICAgdGV4dENvbXBvbmVudC50ZXh0ID0gIjxiPjxjb2xvcj0jMDBFNkNGPiIuLnVpZC4uIjwvY29sb3I+PC9iPiIKICAgICAgICAgICAgZW5kCiAgICAgICAgZW5kCiAgICBlbmQKZW5kCgpiZXRhX3RleHQoKQ=="));
+            await connection.SendPacket(basePacket.BuildPacket());
     }
 }
