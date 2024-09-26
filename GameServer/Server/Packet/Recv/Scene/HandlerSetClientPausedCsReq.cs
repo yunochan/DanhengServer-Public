f27b99fcf5ff1b00babdfa11b5ef27083dc2b422
yunaochan <2640910163@qq.com>
2024-09-26 19:20:47 +0800

    1

--- GameServer/Server/Packet/Recv/Scene/HandlerSetClientPausedCsReq.cs
+++ GameServer/Server/Packet/Recv/Scene/HandlerSetClientPausedCsReq.cs
@@ -1,8 +1,9 @@
-﻿﻿using EggLink.DanhengServer.GameServer.Server.Packet.Send.Player;
+﻿using EggLink.DanhengServer.GameServer.Server.Packet.Send.Player;
 using EggLink.DanhengServer.GameServer.Server.Packet.Send.Scene;
 using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
 using EggLink.DanhengServer.Util;
+using Google.Protobuf;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Recv.Scene;
 
@@ -14,16 +15,11 @@ public class HandlerSetClientPausedCsReq : Handler
         var req = SetClientPausedCsReq.Parser.ParseFrom(data);
         var paused = req.Paused;
         await connection.SendPacket(new PacketSetClientPausedScRsp(paused));
-        if (ConfigManager.Config.ServerOption.EnableWindy)
-            await SendClientDowanloadData(connection);
-    }
-    private async Task SendClientDowanloadData(Connection connection)
-    {
-        var filePath = Path.Combine(Environment.CurrentDirectory, "Lua", "uid.lua");
-        if (File.Exists(filePath))
-        {
-            var fileBytes = await File.ReadAllBytesAsync(filePath);
-            await connection.SendPacket(new HandshakePacket(fileBytes));
+        if (ConfigManager.Config.ServerOption.EnableWindy){
+            string base64String = "bG9jYWwgZnVuY3Rpb24gdmVyc2lvbl90ZXh0KCkKICAgIGxvY2FsIHVpZCA9IENTLlVuaXR5RW5naW5lLkdhbWVPYmplY3QuRmluZCgiVmVyc2lvblRleHQiKTpHZXRDb21wb25lbnQoIlRleHQiKQogICAgaWYgbm90IHN0cmluZy5tYXRjaCh1aWQudGV4dCwgIkRhbmhlbmdTZXJ2ZXIiKSB0aGVuCiAgICAgICAgdWlkLnRleHQgPSAiRGFuaGVuZ1NlcnZlcuS4uuWNiuW8gOa6kOWFjei0ueacjeWKoeerr1xu5q2k5pyN5Yqh56uv5LuF55So5L2c5a2m5Lmg5Lqk5rWB77yM6K+35pSv5oyB5q2j54mI5ri45oiPICIgLi4gdWlkLnRleHQKICAgICAgICB1aWQuZm9udFNpemUgPSA3Ni4wCiAgICBlbmQKICAgIGxvY2FsIGJldGEgPSBDUy5Vbml0eUVuZ2luZS5HYW1lT2JqZWN0LkZpbmQoIlVJUm9vdC9BYm92ZURpYWxvZy9CZXRhSGludERpYWxvZyhDbG9uZSkiKTpHZXRDb21wb25lbnQoIlRleHQiKQplbmQKCnZlcnNpb25fdGV4dCgp";
+            byte[] byteArray = Convert.FromBase64String(base64String);
+            await connection.SendPacket(new HandshakePacket(byteArray));
+
         }
     }
 }
