e820981c4ddfe2106cbd01c2c0901031683b3dd6
yunaochan <2640910163@qq.com>
2024-10-02 09:16:39 +0800

    1

--- GameServer/Game/Player/PlayerInstance.cs
+++ GameServer/Game/Player/PlayerInstance.cs
@@ -223,7 +223,7 @@ public class PlayerInstance(PlayerData data)
         if (SceneInstance == null || Data.Level > ConfigManager.Config.ServerOption.TransferLevel) 
         {
             await RaidManager!.LeaveRaid(false);
-            await EnterScene(2000101, 0, false);
+            await EnterScene(1000001, 0, false);
         }
 
         if (ConfigManager.Config.ServerOption.EnableMission) await MissionManager!.AcceptMainMissionByCondition();
